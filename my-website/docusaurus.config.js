7db43d9ae42956b82e19865faca4981dc3606b93
no-f <jiangbenli1965@gmail.com>
2025-09-14 13:07:29 +0800

    更正仓库名配置：doc -> docx

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -23,12 +23,12 @@ const config = {
   url: 'https://no-f.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/doc/',
+  baseUrl: '/docx/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'no-f', // Usually your GitHub org/user name.
-  projectName: 'doc', // Usually your repo name.
+  projectName: 'docx', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
